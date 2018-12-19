fcb430fdabe80576858e8c28919e2f8c533379e3
Brayden Banks <me@bb010g.com>
2018-12-18 23:54:37 -0800

    Compile on Rust 1.27 (NixOS)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate structopt;
 extern crate xcb;
 use structopt::StructOpt;
@@ -92,7 +93,7 @@ struct Opt {
 
 /// Parse an HTML-color-like hex input
 fn parse_hex(hex: &str) -> Result<u32, String> {
-    let hex = hex.trim_start_matches('#');
+    let hex = hex.trim_left_matches('#');
 
     let mut color;
 
