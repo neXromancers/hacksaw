819bf9b185d2ec31a914f357df196ca9ae0c07d6
expectocode <expectocode@gmail.com>
2019-02-19 17:55:26 +0000

    todo: delete this (nonfunctional)

--- src/main.rs
+++ src/main.rs
@@ -61,31 +61,65 @@ fn contained(x: i16, y: i16, width: i16, height: i16, p_x: i16, p_y: i16) -> boo
     x < p_x && y < p_y && p_x - x <= width && p_y - y <= height
 }
 
+fn is_viewable(conn: &xcb::Connection, window: xcb::Window) -> bool {
+    let attrs = xcb::get_window_attributes(conn, window)
+        .get_reply()
+        .unwrap();
+    (attrs.map_state() & xcb::MAP_STATE_VIEWABLE as u8) != 0
+}
+
+fn point_in_window_geom(
+    conn: &xcb::Connection,
+    window: xcb::Window,
+    x: u32,
+    y: u32,
+) -> Option<(u32, i16, i16, u16, u16)> {
+    let geom = xcb::get_geometry(conn, window).get_reply().unwrap();
+    let (gx, gy, gw, gh): (i16, i16, u16, u16) = (geom.x(), geom.y(), geom.width(), geom.height());
+    if contained(
+        geom.x(),
+        geom.y(),
+        geom.width() as i16,
+        geom.height() as i16,
+        x as i16,
+        y as i16,
+    ) {
+        Some((window, gx, gy, gw, gh))
+    } else {
+        None
+    }
+}
+
 fn get_window_at_point(conn: &xcb::Connection, win: xcb::Window, x: u32, y: u32) {
     let tree = xcb::query_tree(conn, win).get_reply().unwrap();
-    println!("win id {}", win);
-    println!("num children: {}", tree.children_len());
-    for &child in tree.children() {
-        let attrs = xcb::get_window_attributes(conn, child).get_reply().unwrap();
-        let viewable = (attrs.map_state() & xcb::MAP_STATE_VIEWABLE as u8) != 0;
-        if !viewable {
-            continue;
-        }
-        let geom = xcb::get_geometry(conn, child).get_reply().unwrap();
-        let (gx, gy, gw, gh): (i16, i16, u16, u16) =
-            (geom.x(), geom.y(), geom.width(), geom.height());
-        if !contained(
-            geom.x(),
-            geom.y(),
-            geom.width() as i16,
-            geom.height() as i16,
-            x as i16,
-            y as i16,
-        ) {
-            continue;
-        };
-        println!("child geom: {}x{}+{}+{}, id {}", gw, gh, gx, gy, child);
-    }
+    // println!("win id {}", win);
+    // println!("num children: {}", tree.children_len());
+    let (child, gx, gy, gh, gw) = tree
+        .children()
+        .iter()
+        .filter(|&&child| is_viewable(conn, child))
+        .filter_map(|&child| point_in_window_geom(conn, child, x, y))
+        .nth(1)
+        .expect("No viewable window at point!");
+    // let attrs = xcb::get_window_attributes(conn, child).get_reply().unwrap();
+    // let viewable = (attrs.map_state() & xcb::MAP_STATE_VIEWABLE as u8) != 0;
+    // if !viewable {
+    //     continue;
+    // }
+    // let geom = xcb::get_geometry(conn, child).get_reply().unwrap();
+    // let (gx, gy, gw, gh): (i16, i16, u16, u16) = (geom.x(), geom.y(), geom.width(), geom.height());
+    // if !contained(
+    //     geom.x(),
+    //     geom.y(),
+    //     geom.width() as i16,
+    //     geom.height() as i16,
+    //     x as i16,
+    //     y as i16,
+    // ) {
+    //     continue;
+    // };
+    // println!("child geom: {}x{}+{}+{}, id {}", gw, gh, gx, gy, child);
+    println!("{}x{}+{}+{}", gw, gh, gx, gy);
 }
 
 #[derive(StructOpt, Debug)]
@@ -260,9 +294,10 @@ fn main() {
                 right_x = x.max(start_x);
                 bot_y = y.max(start_y);
 
-                width = (x - start_x).abs() as u16;
-                height = (y - start_y).abs() as u16;
-
+                if in_selection {
+                    width = (x - start_x).abs() as u16;
+                    height = (y - start_y).abs() as u16;
+                }
                 let mut rects = match (opt.no_guides, in_selection) {
                     (_, true) => vec![
                         // Selection rectangle
@@ -316,8 +351,9 @@ fn main() {
     if width == 0 && height == 0 {
         // Grab window under cursor
         get_window_at_point(&conn, screen.root(), start_x as u32, start_y as u32);
-        get_window_at_point(&conn, 2097153, start_x as u32, start_y as u32);
+    // get_window_at_point(&conn, 2097153, start_x as u32, start_y as u32);
+    } else {
+        println!("{}x{}+{}+{}", width, height, left_x, top_y);
     }
     // Now we have taken coordinates, we print them out
-    println!("{}x{}+{}+{}", width, height, left_x, top_y);
 }
