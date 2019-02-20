3e6535dbcd93f73c065bb33b7a2fbc2f709b9791
expectocode <expectocode@gmail.com>
2019-02-20 10:07:17 +0000

    more correct window indexing

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn viewable(conn: &xcb::Connection, win: xcb::Window) -> bool {
 
 fn get_window_at_point(conn: &xcb::Connection, win: xcb::Window, x: i16, y: i16) -> Geom {
     let tree = xcb::query_tree(conn, win).get_reply().unwrap();
-    tree.children()
+    let geoms = tree.children()
         .iter()
         .filter(|&child| viewable(conn, *child))
         .filter_map(|&child| {
@@ -95,8 +95,8 @@ fn get_window_at_point(conn: &xcb::Connection, win: xcb::Window, x: i16, y: i16)
                 None
             }
         })
-        .nth(1)
-        .expect("Could not retrieve window geometry under cursor")
+    .collect::<Vec<_>>();
+    geoms[geoms.len() - 2]
 }
 
 #[derive(StructOpt, Debug)]
