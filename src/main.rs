dd4a35dcd66d5053e87e99a8fd342d9a23ef59e6
expectocode <expectocode@gmail.com>
2019-02-21 22:44:53 +0000

    Revert "more correct window indexing"
    This reverts commit 3e6535dbcd93f73c065bb33b7a2fbc2f709b9791.

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn viewable(conn: &xcb::Connection, win: xcb::Window) -> bool {
 
 fn get_window_at_point(conn: &xcb::Connection, win: xcb::Window, x: i16, y: i16) -> Geom {
     let tree = xcb::query_tree(conn, win).get_reply().unwrap();
-    let geoms = tree
+    tree
         .children()
         .iter()
         .filter(|&child| viewable(conn, *child))
@@ -89,8 +89,8 @@ fn get_window_at_point(conn: &xcb::Connection, win: xcb::Window, x: i16, y: i16)
                 None
             }
         })
-        .collect::<Vec<_>>();
-    geoms[geoms.len() - 2]
+        .nth(1)
+        .expect("Could not retrieve window geometry under cursor")
 }
 
 #[derive(StructOpt, Debug)]
