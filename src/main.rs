9e5ed07ac2f333bc7905620c0ff82b2911120ad3
expectocode <expectocode@gmail.com>
2019-06-19 19:08:34 +0100

    Use custom hex error struct

--- src/main.rs
+++ src/main.rs
@@ -128,34 +128,50 @@ struct Opt {
     line_colour: u32,
 }
 
-/// Parse an HTML-color-like hex input
-fn parse_hex(hex: &str) -> Result<u32, String> {
-    let hex = hex.trim_start_matches('#');
+#[derive(Debug)]
+struct ParseHexError {
+    reason: String
+}
 
+impl std::fmt::Display for ParseHexError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.reason)
+    }
+}
+
+impl From<std::num::ParseIntError> for ParseHexError {
+    fn from(err: std::num::ParseIntError) -> ParseHexError {
+        ParseHexError{reason: format!("{}", err)}
+    }
+}
+
+/// Parse an HTML-color-like hex input
+fn parse_hex(hex: &str) -> Result<u32, ParseHexError> {
+    let hex = hex.trim_start_matches('#');
     let mut color;
 
     match hex.len() {
         3 | 4 => {
-            color = 0x11 * u32::from_str_radix(&hex[2..3], 16).expect("Invalid hex")
-                + 0x11_00 * u32::from_str_radix(&hex[1..2], 16).expect("Invalid hex")
-                + 0x11_00_00 * u32::from_str_radix(&hex[0..1], 16).expect("Invalid hex");
+            color = 0x11 * u32::from_str_radix(&hex[2..3], 16)?
+                + 0x11_00 * u32::from_str_radix(&hex[1..2], 16)?
+                + 0x11_00_00 * u32::from_str_radix(&hex[0..1], 16)?;
 
             if hex.len() == 4 {
-                color |= 0x11_00_00_00 * u32::from_str_radix(&hex[3..4], 16).expect("Invalid hex");
+                color |= 0x11_00_00_00 * u32::from_str_radix(&hex[3..4], 16)?
             } else {
                 color |= 0xFF_00_00_00;
             }
         }
 
         6 | 8 => {
-            color = u32::from_str_radix(&hex, 16).expect("Invalid hex");
+            color = u32::from_str_radix(&hex, 16)?;
 
             if hex.len() == 6 {
                 color |= 0xFF_00_00_00;
             }
         }
 
-        _ => return Err("Bad hex colour".to_string()),
+        _ => return Err(ParseHexError{reason: "Bad hex colour".to_owned()}),
     }
 
     Ok(color)
